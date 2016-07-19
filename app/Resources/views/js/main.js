d1071c8c797a6f368699a2da6888ca6c9708b94c
joseph <joseph.devarenne@viacesi.fr>
2016-07-19 09:20:25 +0200

    view

---
+++ app/Resources/views/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function()
+{
+	var menuId = $( "ul.nav" ).first().attr( "id" );
+	var request = $.ajax({
+		url: "script.php",
+		method: "POST",
+		data: { id : menuId },
+		dataType: "html"
+	});
+
+	request.done(function( msg ) {
+		$( "#log" ).html( msg );
+	});
+
+	request.fail(function( jqXHR, textStatus ) {
+		alert( "Request failed: " + textStatus );
+	});
+});
